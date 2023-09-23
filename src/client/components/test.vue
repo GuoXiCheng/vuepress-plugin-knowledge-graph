7d5b464bfc29265e03b4fe00db884360b9168478
Xicheng Guo <xicheng_guo@wistron.com>
2023-09-23 16:01:09 +0800

    add vue component

--- src/client/components/test.vue
+++ src/client/components/test.vue
@@ -1,16 +1,17 @@
 <template>
-  <header>
-    <h1>这是一个自定义的标题</h1>
-    <p>这是一些自定义内容</p>
-  </header>
+    <span @click="clickSpan">@</span>
 </template>
 
 <script>
 export default {
-  // 组件逻辑
-}
+    methods: {
+        clickSpan() {
+            console.log("span clicked");
+        },
+    },
+};
 </script>
 
 <style scoped>
-/* 组件样式 */
+
 </style>
--- src/client/config.ts
+++ src/client/config.ts
@@ -1,20 +1,21 @@
 import { defineClientConfig } from '@vuepress/client'
 import TestComponent from './components/test.vue';
-import { createApp } from 'vue';
+import { createApp, onMounted } from 'vue';
 
 export default defineClientConfig({
     enhance({ app }) {
 
     },
     setup() {
-        setTimeout(()=>{
+        // 在组件挂载后执行 DOM 操作
+        onMounted(() => {
             const headerElement = document.getElementsByTagName('header')[0];
             const newElement = document.createElement('div');
             headerElement.appendChild(newElement);
+
+            // 在新元素上创建子组件
             const instance = createApp(TestComponent);
             instance.mount(newElement);
-        }, 1000)
-
+        });
     }
-  });
-  
+});
