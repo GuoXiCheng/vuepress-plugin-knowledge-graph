80c7dfe65cc75b7112bd02ef88a147c1079c18f7
Xicheng Guo <xicheng_guo@wistron.com>
2023-09-23 15:29:16 +0800

    update client config

--- src/client/config.ts
+++ src/client/config.ts
@@ -1,11 +1,20 @@
 import { defineClientConfig } from '@vuepress/client'
+import TestComponent from './components/test.vue';
+import { createApp } from 'vue';
 
 export default defineClientConfig({
     enhance({ app }) {
-      
+
     },
     setup() {
-        console.log('setup')
+        setTimeout(()=>{
+            const headerElement = document.getElementsByTagName('header')[0];
+            const newElement = document.createElement('div');
+            headerElement.appendChild(newElement);
+            const instance = createApp(TestComponent);
+            instance.mount(newElement);
+        }, 1000)
+
     }
   });
   
