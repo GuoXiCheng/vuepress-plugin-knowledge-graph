2dc62664f1f5a53511f1eb06723c8e14331c69f8
GuoXiCheng <1377994267@qq.com>
2023-10-05 22:04:42 +0800

    update test

--- src/test/utils.test.ts
+++ src/test/utils.test.ts
@@ -1,4 +1,5 @@
-import { getTempContent } from '../utils';
+import { Page } from 'vuepress';
+import { getDocNodes, getTempContent } from '../utils';
 describe('getTempContent', () => {
   test('should return a string', () => {
     const detail = getTempContent({ name: "docNodes", value: "test string" });
@@ -10,8 +11,65 @@ describe('getTempContent', () => {
     expect(detail).toEqual('export const docLinks = {"a":1,"b":2}');
   });
 
-  test('other', ()=>{
-    const detail = getTempContent({ name: "docLinks", value: true});
+  test('other', () => {
+    const detail = getTempContent({ name: "docLinks", value: true });
     expect(detail).toEqual('export const docLinks = true');
+  });
+});
+
+describe('getDocNodes', () => {
+  test('excludeReadme is true', () => {
+    const detail = getDocNodes([{
+      filePath: 'Readme.md',
+    }, {
+      filePath: 'D:/test/README.md'
+    }, {
+      filePath: 'D:/test/test.md'
+    }] as Page[], { excludeReadme: true });
+    expect(detail).toEqual([{ id: 'test', label: 'test' }]);
+  });
+
+  test('excludeReadme is not true', () => {
+    const detail = getDocNodes([{
+      filePath: 'Readme.md',
+    }, {
+      filePath: 'D:/test/README.md'
+    }, {
+      filePath: 'D:/test/test.md'
+    }] as Page[], {});
+    expect(detail).toEqual([{ id: 'README', label: 'README' }, { id: 'test', label: 'test' }])
+  });
+
+  test('only markdown', () => {
+    const detail = getDocNodes([{
+      filePath: 'D:/test/test/test.md',
+    }, {
+      filePath: 'D:/test/test.js'
+    }, {
+      filePath: 'D:/test/test.ts'
+    }] as Page[], {});
+    expect(detail).toEqual([{ id: 'test', label: 'test' }]);
+  });
+
+  test('exclude', () => {
+    const detail = getDocNodes([{
+      filePath: 'D:/guide/test/test1.md',
+    }, {
+      filePath: 'D:/demo/text2.md'
+    }, {
+      filePath: 'D:/test/test3.md'
+    }] as Page[], { exclude: ['test'] });
+    expect(detail).toEqual([{ id: 'text2', label: 'text2' }])
+  });
+
+  test('exclude and include', () => {
+    const detail = getDocNodes([{
+      filePath: 'demo/text1.md',
+    }, {
+      filePath: 'demo/text2.md'
+    }, {
+      filePath: 'test/test3.md'
+    }] as Page[], { exclude: ['test', 'demo'], include: ['demo/text1.md'] });
+    expect(detail).toEqual([{ id: 'text1', label: 'text1' }])
   });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,6 @@
 import { TempFileKey } from "@temp/knowledge-graph-data";
+import { KnowledgeGraphOption } from "knowledge-graph-types";
+import { Page } from "vuepress";
 
 export function getTempContent(...contents: Array<{ name: TempFileKey, value: any }>) {
     const temp = [];
@@ -18,4 +20,37 @@ export function getTempContent(...contents: Array<{ name: TempFileKey, value: an
         temp.push(`export const ${content.name} = ${typeValue}`);
     }
     return temp.join('\n');
+}
+
+export function getDocNodes(pages: Page[], options: KnowledgeGraphOption) {
+    const regexPattern = /\/([^/]+)\.md$/;
+    const nodes = pages.filter((item: Page) => {
+
+        if (typeof (options.excludeReadme) === 'boolean'
+            && options.excludeReadme === true
+            && item.filePath?.toLowerCase().endsWith('readme.md')) {
+            return false;
+        }
+
+        const match = item.filePath?.match(regexPattern);
+        if (match == null) {
+            return false;
+        }
+
+        if (Array.isArray(options.include) && options.include.length !== 0) {
+            const isInclude = options.include.some(pattern => item.filePath?.startsWith(pattern));
+            if (isInclude) return true;
+        }
+
+        if (Array.isArray(options.exclude) && options.exclude.length !== 0) {
+            return !options.exclude.some(pattern => item.filePath?.includes(pattern));
+        }
+        return true;
+    }).map((item: Page) => {
+        const match = item.filePath?.match(regexPattern) as RegExpMatchArray;
+        return {
+            id: match[1], label: match[1]
+        }
+    });
+    return nodes;
 }
